792448a336c98145abcce98ea3318f949f6759a9
anatawa12 <anatawa12@icloud.com>
2022-05-23 10:25:53 +0900

    rewrite with cl4e

--- Relocator.cs
+++ Relocator.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using CustomLocalization4EditorExtension;
 using UnityEditor;
 using UnityEngine;
 using VRC.SDK3.Dynamics.PhysBone.Components;
@@ -9,45 +9,12 @@ public class RelocatorWindow : EditorWindow
     private static GameObject _source;
     private bool _wasMissingDetected;
 
+    private Localization _localization = new Localization(
+        "6384038aaded94ab598437c5a398be61", "ja");
+
     private const string Version = "0.1.0";
-    private static string _lang = "en-US";
 
-    private static readonly Dictionary<string, Dictionary<string, string>> Texts =
-        new Dictionary<string, Dictionary<string, string>>()
-        {
-            {
-                "en-US", new Dictionary<string, string>()
-                {
-                    { "langSwitch", "Switch language to Japanese/日本語にする" },
-                    {
-                        "summary",
-                        "Relocate the PhysBone and PhysBoneCollider Components to the object set in the \"Root Transform\".\nSet a GameObject which has the target PhysBone or PhysBoneCollider Components, and press \"Relocate!\"."
-                    },
-                    { "msgWhenError", "\n\nIf this error is unexpected, please contact the author of this extension." },
-                    { "source", "Target GameObject" },
-                    { "noSource", "You need to set a GameObject." },
-                    { "Succeeded-Missing", "Succeeded, but missing was detected.\nIt is recommended to check." },
-                    { "Succeeded", "Succeeded!" }
-                }
-            },
-            {
-                "ja-JP", new Dictionary<string, string>()
-                {
-                    { "langSwitch", "Switch language to English/英語にする" },
-                    {
-                        "summary",
-                        "PhysBoneコンポーネント類を\"Root Transform\"に設定されているゲームオブジェクトに再配置します。\n対象のPhysBoneコンポーネント類を含むゲームオブジェクトをセットし、\"Relocate!\"ボタンを押してください。"
-                    },
-                    { "msgWhenError", "\n\nこのエラーに心当たりが無い場合は、この拡張の作者に連絡してみてください。" },
-                    { "source", "対象のゲームオブジェクト" },
-                    { "noSource", "ゲームオブジェクトをセットする必要があります。" },
-                    { "Succeeded-Missing", "成功しましたが、Root Transformがmissingになっているオブジェクトが見つかりました。\n確認することをお勧めします。" },
-                    { "Succeeded", "成功！" }
-                }
-            }
-        };
-
-//methods
+    //methods
     [MenuItem("Tools/PB Relocator", false, 1)]
     public static void ShowWindow()
     {
@@ -56,17 +23,16 @@ public class RelocatorWindow : EditorWindow
 
     private void OnGUI()
     {
-        if (GUILayout.Button(Texts[_lang]["langSwitch"]))
-        {
-            _lang = _lang == "en-US" ? "ja-JP" : "en-US";
-        }
+        _localization.Setup();
+        _localization.DrawLanguagePicker();
 
-        EditorGUILayout.LabelField(Texts[_lang]["summary"], EditorStyles.wordWrappedLabel);
+        EditorGUILayout.LabelField(_localization.Tr("summary"), EditorStyles.wordWrappedLabel);
 
-        _source = (GameObject)EditorGUILayout.ObjectField(Texts[_lang]["source"], _source, typeof(GameObject), true);
+        _source = (GameObject)EditorGUILayout.ObjectField(_localization.Tr("source"), _source, typeof(GameObject),
+            true);
         if (_source == null)
         {
-            EditorGUILayout.LabelField(Texts[_lang]["noSource"]);
+            EditorGUILayout.LabelField(_localization.Tr("noSource"));
         }
         else if (GUILayout.Button("Relocate!"))
         {
@@ -83,17 +49,17 @@ public class RelocatorWindow : EditorWindow
 
                 if (_wasMissingDetected)
                 {
-                    EditorUtility.DisplayDialog("Result", Texts[_lang]["Succeeded-Missing"], "OK");
+                    EditorUtility.DisplayDialog("Result", _localization.Tr("Succeeded-Missing"), "OK");
                     _wasMissingDetected = false;
                 }
                 else
                 {
-                    EditorUtility.DisplayDialog("Result", Texts[_lang]["Succeeded"], "OK");
+                    EditorUtility.DisplayDialog("Result", _localization.Tr("Succeeded"), "OK");
                 }
             }
             catch (System.Exception e)
             {
-                EditorUtility.DisplayDialog("Error", e.Message + Texts[_lang]["msgWhenError"], "OK");
+                EditorUtility.DisplayDialog("Error", e.Message + _localization.Tr("msgWhenError"), "OK");
                 Debug.LogError(e);
             }
         }
